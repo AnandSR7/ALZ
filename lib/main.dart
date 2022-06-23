5a265a9ad9317e91f3d14939958bf2a5f839bdbb
Amildenine <82970582+Amildenine@users.noreply.github.com>
2022-06-23 23:03:58 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:login_singup/screen/login_signup.dart';
-
+import 'home.dart';
+import 'package:flutter/services.dart';
 void main() {
-  runApp(LoginSignupUI());
-}
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
 
-class LoginSignupUI extends StatelessWidget {
-  const LoginSignupUI({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Login Signup UI",
-      home: LoginSignupScreen(),
-    );
-  }
+  runApp(
+    MyApp()
+  );
 }
